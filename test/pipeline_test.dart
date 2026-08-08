9de3976e599d81e937ede97ccc8a918fd44fb5e8
winetree94 <me@winetree94.com>
2026-08-09 00:06:38 +0900

    Fix CocoaPods guard test lint

--- test/pipeline_test.dart
+++ test/pipeline_test.dart
@@ -222,7 +222,7 @@ void main() {
       contains("config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'"),
     );
     expect(iosPodfile, contains('post_integrate do'));
-    expect(iosPodfile, contains("-l\\\"?Pods-Runner\\\"?"));
+    expect(iosPodfile, contains(r'-l\"?Pods-Runner\"?'));
     expect(iosProject, isNot(contains('FlutterGeneratedPluginSwiftPackage')));
   });
 
