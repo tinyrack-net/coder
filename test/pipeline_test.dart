bd530c24cb41dfeec16292c3285951beb849af83
winetree94 <me@winetree94.com>
2026-08-09 00:16:35 +0900

    Persist CocoaPods aggregate link cleanup

--- test/pipeline_test.dart
+++ test/pipeline_test.dart
@@ -221,8 +221,7 @@ void main() {
       iosPodfile,
       contains("config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'"),
     );
-    expect(iosPodfile, contains('post_integrate do'));
-    expect(iosPodfile, contains(r'-l\"?Pods-Runner\"?'));
+    expect(iosPodfile, contains("config_file.libraries.delete('Pods-Runner')"));
     expect(iosProject, isNot(contains('FlutterGeneratedPluginSwiftPackage')));
   });
 
